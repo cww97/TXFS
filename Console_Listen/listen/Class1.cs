6bb006aa5d2ae01d22f2a02117665aaf089a8dbe
殷正航 <10142130151_ecnu@outlook.com>
2016-11-27 06:02:52 +0800

    Basic socket completed!

---
+++ Console_Listen/listen/Class1.cs
@@ -0,0 +1,350 @@
+﻿namespace Microsoft.CognitiveServices.SpeechRecognition
+{
+    using System;
+    using System.ComponentModel;
+    using System.Configuration;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Runtime.CompilerServices;
+    using System.Windows;
+    public class Listen
+    {
+        private string subscriptionKey = "68aca18d6fad41ae8f3ed8a633652f17";
+        public string result = "";
+        /// <summary>
+        /// The data recognition client
+        /// </summary>
+        private DataRecognitionClient dataClient;
+
+        /// <summary>
+        /// The microphone client
+        /// </summary>
+        private MicrophoneRecognitionClient micClient;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
+        public string output()
+        {
+            Listen x = new Listen();
+            x.Initialize();
+            x.StartButton_Click(new Object(), new RoutedEventArgs());
+            return result;
+        }
+        /// <summary>
+        /// Gets or sets subscription key
+        /// </summary>
+        public string SubscriptionKey
+        {
+            get
+            {
+                return this.subscriptionKey;
+            }
+
+            set
+            {
+                this.subscriptionKey = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets the LUIS application identifier.
+        /// </summary>
+        /// <value>
+        /// The LUIS application identifier.
+        /// </value>
+        private string LuisAppId = "yourLuisAppID";
+        /// <summary>
+        /// Gets the LUIS subscription identifier.
+        /// </summary>
+        /// <value>
+        /// The LUIS subscription identifier.
+        /// </value>
+        private string LuisSubscriptionID = "yourLuisSubsrciptionID";
+        /// Gets the current speech recognition mode.
+        /// </summary>
+        /// <value>
+        /// The speech recognition mode.
+        /// </value>
+        private SpeechRecognitionMode Mode = SpeechRecognitionMode.ShortPhrase;
+
+
+        /// <summary>
+        /// Gets the default locale.
+        /// </summary>
+        /// <value>
+        /// The default locale.
+        /// </value>
+        private string DefaultLocale = "zh-CN";
+
+
+        /// <summary>
+        /// Initializes a fresh audio session.
+        /// </summary>
+        private void Initialize()
+        {
+            this.SubscriptionKey = "68aca18d6fad41ae8f3ed8a633652f17";
+        }
+
+        /// <summary>
+        /// Handles the Click event of the _startButton control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            this.WriteLine("start");
+            this.CreateMicrophoneRecoClientWithIntent();
+            this.micClient.StartMicAndRecognition();
+
+        }
+
+
+
+        /// <summary>
+        /// Creates a new microphone reco client without LUIS intent support.
+        /// </summary>
+        private void CreateMicrophoneRecoClient()
+        {
+            this.micClient = SpeechRecognitionServiceFactory.CreateMicrophoneClient(
+                SpeechRecognitionMode.ShortPhrase,
+                "en - US",
+                this.SubscriptionKey);
+
+            // Event handlers for speech recognition results
+            this.micClient.OnMicrophoneStatus += this.OnMicrophoneStatus;
+            this.micClient.OnPartialResponseReceived += this.OnPartialResponseReceivedHandler;
+            if (this.Mode == SpeechRecognitionMode.ShortPhrase)
+            {
+                this.micClient.OnResponseReceived += this.OnMicShortPhraseResponseReceivedHandler;
+            }
+
+        }
+
+        /// <summary>
+        /// Creates a new microphone reco client with LUIS intent support.
+        /// </summary>
+        private void CreateMicrophoneRecoClientWithIntent()
+        {
+            this.micClient =
+                SpeechRecognitionServiceFactory.CreateMicrophoneClientWithIntent(
+                this.DefaultLocale,
+                this.SubscriptionKey,
+                this.LuisAppId,
+                this.LuisSubscriptionID);
+            this.micClient.OnIntent += this.OnIntentHandler;
+
+            // Event handlers for speech recognition results
+            this.micClient.OnMicrophoneStatus += this.OnMicrophoneStatus;
+            this.micClient.OnPartialResponseReceived += this.OnPartialResponseReceivedHandler;
+            this.micClient.OnResponseReceived += this.OnMicShortPhraseResponseReceivedHandler;
+
+        }
+
+        /// <summary>
+        /// Creates a data client without LUIS intent support.
+        /// Speech recognition with data (for example from a file or audio source).  
+        /// The data is broken up into buffers and each buffer is sent to the Speech Recognition Service.
+        /// No modification is done to the buffers, so the user can apply their
+        /// own Silence Detection if desired.
+        /// </summary>
+        private void CreateDataRecoClient()
+        {
+            this.dataClient = SpeechRecognitionServiceFactory.CreateDataClient(
+                this.Mode,
+                this.DefaultLocale,
+                this.SubscriptionKey);
+
+            // Event handlers for speech recognition results
+
+            this.dataClient.OnResponseReceived += this.OnDataDictationResponseReceivedHandler;
+
+
+            this.dataClient.OnPartialResponseReceived += this.OnPartialResponseReceivedHandler;
+
+        }
+
+        /// <summary>
+        /// Creates a data client with LUIS intent support.
+        /// Speech recognition with data (for example from a file or audio source).  
+        /// The data is broken up into buffers and each buffer is sent to the Speech Recognition Service.
+        /// No modification is done to the buffers, so the user can apply their
+        /// own Silence Detection if desired.
+        /// </summary>
+        private void CreateDataRecoClientWithIntent()
+        {
+            this.dataClient = SpeechRecognitionServiceFactory.CreateDataClientWithIntent(
+                this.DefaultLocale,
+                this.SubscriptionKey,
+                this.LuisAppId,
+                this.LuisSubscriptionID);
+
+
+            this.dataClient.OnPartialResponseReceived += this.OnPartialResponseReceivedHandler;
+
+
+            // Event handler for intent result
+            this.dataClient.OnIntent += this.OnIntentHandler;
+        }
+
+        /// <summary>
+        /// Sends the audio helper.
+        /// </summary>
+        /// <param name="wavFileName">Name of the wav file.</param>
+        private void SendAudioHelper(string wavFileName)
+        {
+            using (FileStream fileStream = new FileStream(wavFileName, FileMode.Open, FileAccess.Read))
+            {
+                // Note for wave files, we can just send data from the file right to the server.
+                // In the case you are not an audio file in wave format, and instead you have just
+                // raw data (for example audio coming over bluetooth), then before sending up any 
+                // audio data, you must first send up an SpeechAudioFormat descriptor to describe 
+                // the layout and format of your raw audio data via DataRecognitionClient's sendAudioFormat() method.
+                int bytesRead = 0;
+                byte[] buffer = new byte[1024];
+
+                try
+                {
+                    do
+                    {
+                        // Get more Audio data to send into byte buffer.
+                        bytesRead = fileStream.Read(buffer, 0, buffer.Length);
+
+                        // Send of audio data to service. 
+                        this.dataClient.SendAudio(buffer, bytesRead);
+                    }
+                    while (bytesRead > 0);
+                }
+                finally
+                {
+                    // We are done sending audio.  Final recognition results will arrive in OnResponseReceived event call.
+                    this.dataClient.EndAudio();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Called when a final response is received;
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="SpeechResponseEventArgs"/> instance containing the event data.</param>
+        private void OnMicShortPhraseResponseReceivedHandler(object sender, SpeechResponseEventArgs e)
+        {
+
+
+            // we got the final result, so it we can end the mic reco.  No need to do this
+            // for dataReco, since we already called endAudio() on it as soon as we were done
+            // sending all the data.
+            this.micClient.EndMicAndRecognition();
+
+            this.WriteResponseResult(e);
+        }
+
+
+
+        /// <summary>
+        /// Writes the response result.
+        /// </summary>
+        /// <param name="e">The <see cref="SpeechResponseEventArgs"/> instance containing the event data.</param>
+        private void WriteResponseResult(SpeechResponseEventArgs e)
+        {
+            string tmp = "";
+            if (e.PhraseResponse.Results.Length == 0)
+            {
+                this.WriteLine("No phrase response is available.");
+            }
+            else
+            {
+                for (int i = 0; i < e.PhraseResponse.Results.Length; i++)
+                {
+                    /*this.WriteLine(
+                        "[{0}]",
+                        e.PhraseResponse.Results[i].DisplayText);*/
+                    tmp = tmp.Insert(tmp.Length, e.PhraseResponse.Results[i].DisplayText);
+                    tmp = tmp.Insert(tmp.Length, "\n");
+                }
+                result = tmp;
+                //Console.Out.Write(tmp);
+            }
+        }
+        /// <summary>
+        /// Called when a final response is received;
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="SpeechResponseEventArgs"/> instance containing the event data.</param>
+        private void OnDataDictationResponseReceivedHandler(object sender, SpeechResponseEventArgs e)
+        {
+
+
+            this.WriteResponseResult(e);
+        }
+
+        /// <summary>
+        /// Called when a final response is received and its intent is parsed
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="SpeechIntentEventArgs"/> instance containing the event data.</param>
+        private void OnIntentHandler(object sender, SpeechIntentEventArgs e)
+        {
+            this.WriteLine("{0}", e.Payload);
+            this.WriteLine();
+        }
+
+        /// <summary>
+        /// Called when a partial response is received.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="PartialSpeechResponseEventArgs"/> instance containing the event data.</param>
+        private void OnPartialResponseReceivedHandler(object sender, PartialSpeechResponseEventArgs e)
+        {
+            this.WriteLine("{0}", e.PartialResult);
+            this.WriteLine();
+        }
+        /// <summary>
+        /// Called when the microphone status has changed.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="MicrophoneEventArgs"/> instance containing the event data.</param>
+        private void OnMicrophoneStatus(object sender, MicrophoneEventArgs e)
+        {
+            if (e.Recording)
+            {
+                WriteLine("Please start speaking.");
+            }
+
+            WriteLine();
+
+        }
+
+        /// <summary>
+        /// Writes the line.
+        /// </summary>
+        private void WriteLine()
+        {
+            this.WriteLine(string.Empty);
+        }
+
+        /// <summary>
+        /// Writes the line.
+        /// </summary>
+        /// <param name="format">The format.</param>
+        /// <param name="args">The arguments.</param>
+        private void WriteLine(string format, params object[] args)
+        {
+            var formattedStr = string.Format(format, args);
+            Trace.WriteLine(formattedStr);
+            Console.Out.WriteLine(formattedStr);
+        }
+
+        /// <summary>
+        /// Gets the subscription key from isolated storage.
+        /// </summary>
+        /// <returns>The subscription key.</returns>
+        private string GetSubscriptionKeyFromIsolatedStorage()
+        {
+            return "68aca18d6fad41ae8f3ed8a633652f17";
+        }
+
+    }
+}
---
+++ TXFSAnalyze/TXFSAnalyze/Analyze/AnalyzeSocket.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
+using System.IO;
+
+namespace TXFSAnalyze.Analyze
+{
+    class AnalyzeSocket
+    {
+        static readonly string url_format =
+            "https://api.projectoxford.ai/luis/v2.0/apps/17ddee34-fad7-4448-bd72-0344fb2a14ef?subscription-key=b935c555d7cb4dc7b7be132c0425afef&verbose=true&q={0}";
+
+        public async static Task<result_class> query(string query_string)
+        {
+            var http = new HttpClient();
+            var response = await http.GetAsync(string.Format(url_format, query_string));
+            var result = await response.Content.ReadAsStringAsync();
+            var serializer = new DataContractJsonSerializer(typeof(result_class));
+
+            var memstream = new MemoryStream(Encoding.UTF8.GetBytes(result));
+            var data = (result_class)serializer.ReadObject(memstream);
+
+            return data;
+        }
+    }
+
+    [DataContract]
+    public class intent_action_class
+    {
+        [DataMember]
+        public bool triggered { get; set; }
+        [DataMember]
+        public string name { get; set; }
+        [DataMember]
+        public string[] parameters { get; set; }
+    }
+
+    [DataContract]
+    public class intent_class
+    {
+        [DataMember]
+        public string intent { get; set; }
+        [DataMember]
+        public double score { get; set; }
+        [DataMember]
+        public intent_action_class[] actions { get; set; }
+    }
+
+    [DataContract]
+    public class entity_resolution_class
+    {
+        [DataMember]
+        public string date { get; set; }
+    }
+
+    [DataContract]
+    public class entity_class
+    {
+        [DataMember]
+        public string entity { get; set; }
+        [DataMember]
+        public string type { get; set; }
+        [DataMember]
+        public int startIndex { get; set; }
+        [DataMember]
+        public int endIndex { get; set; }
+        [DataMember]
+        public double score { get; set; }
+        [DataMember]
+        public entity_resolution_class resolution { get; set; }
+    }
+
+    [DataContract]
+    public class result_class
+    {
+        [DataMember]
+        public string query { get; set; }
+        [DataMember]
+        public intent_class topScoringIntent { get; set; }
+        [DataMember]
+        public intent_class[] intents { get; set; }
+        [DataMember]
+        public entity_class[] entities { get; set; }
+    }
+}
